812e07812de9fe5fad64b9c55cb94a0df1430dec
Thomas <trichmond@vigiltechnologies.com>
2019-07-12 22:32:22 -0700

    Added contact support to the character sheet. Added dynamic contact listing to the interface

--- interface.js
+++ interface.js
@@ -4,8 +4,7 @@ var characterSheet;
 $(document).ready(function(){
 	characterSheet = new CharacterSheet();
 
-	document.getElementById("addSkill").onclick = function() {
-		
+	document.getElementById("addSkill").onclick = function() {	
 		do {
 			var skillName = window.prompt("Enter a Skill:", "");
 			if (skillName === "") alert("Please enter a Valid Skill Name");
@@ -31,5 +30,37 @@ $(document).ready(function(){
 		skillElement.appendChild(score);
 		
 		document.getElementById("skillstable").appendChild(skillElement);
+	};
+	
+	document.getElementById("contactstable").onclick = function() {	
+		do {
+			var contactName = window.prompt("Enter a Contac Name:", "");
+			if (contactName === "") alert("Please enter a Valid Skill Name");
+		} while (contactName === "");
+		var contactConnection = window.prompt("Enter a Connection Rating:", "");
+		var contactLoyalty = window.prompt("Enter a Loyalty Rating:", "");
+		
+		var contact = characterSheet.addContact(contactName, contactConnection, contactLoyalty);
+		
+		var contactElement1 = document.createElement("tr");
+		var contactNameElement = document.createElement("th");
+		contactNameElement.rowSpan = "2";
+		contactNameElement.appendChild(document.createTextNode(contact.name));
+		var contactConnectionElement = document.createElement("td");
+		contactConnectionElement.appendChild(document.createTextNode("Connection: " + contact.connection));
+		
+		contactElement1.appendChild(document.createElement("th"));
+		contactElement1.appendChild(contactNameElement);
+		contactElement1.appendChild(contactConnectionElement);
+		
+		var contactElement2 = document.createElement("tr");
+		var contactLoyaltyElement = document.createElement("td");
+		contactLoyaltyElement.appendChild(document.createTextNode("Loyalty:" + contact.loyalty));
+		
+		contactElement2.appendChild(document.createElement("th"));
+		contactElement2.appendChild(contactLoyaltyElement);
+		
+		document.getElementById("contactstable").appendChild(contactElement1);
+		document.getElementById("contactstable").appendChild(contactElement2);	
 	};
 });
